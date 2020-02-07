28a21eb24ffd98cf570d5243d3bec140e0f6f5af
Terrell <terrell@mini.local>
2020-02-07 15:22:39 +0800

    跳转iM 订单界面

--- assets/subgame/pay/Script/Recharge/RgDc/payTipAlert.ts
+++ assets/subgame/pay/Script/Recharge/RgDc/payTipAlert.ts
@@ -19,6 +19,18 @@ export default class NewClass extends cc.Component {
     onClick(){
         //按键音效
         this.app.clickClip.play();
+        this.intoIncompleteOrder()
+    }
+    intoIncompleteOrder(){
+        let imHost = this.app.UrlData.imHost;
+        let url = `${imHost}/im/api/intoIncompleteOrder`;
+        let dataStr = `user_id=${this.app.UrlData.user_id}`
+        let self = this;
+        cc.log('intoIncompleteOrder',url,'dataStr',dataStr)
+        this.app.ajax('POST',url,dataStr,()=>{
+        },(errstatus)=>{
+            self.app.showAlert(`网络错误${errstatus}`)
+        })
         // 唤起IM
         gHandler.Reflect.setOrientation("portrait", 640, 1136)
         cc.director.loadScene('IMappStart');
