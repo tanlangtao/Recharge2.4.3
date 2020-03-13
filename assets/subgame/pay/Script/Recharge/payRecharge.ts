cc22da48fce58fbfe400163fdf63f641b34ead98
谭浪涛 <tanlangtao@terrell.local>
2020-03-13 10:44:01 +0800

    添加充值左侧导航栏配置

--- assets/subgame/pay/Script/Recharge/payRecharge.ts
+++ assets/subgame/pay/Script/Recharge/payRecharge.ts
@@ -88,32 +88,62 @@ export default class NewClass extends cc.Component {
     }
 
     public addNavToggle() {
-        var arr = ['人工代充值'];
-        
-        if (this.zfbResults.data.bankcard_transfer.length > 0 ) {
-            arr.push('转账到银行卡')
+        let payment_mode_sort = this.zfbResults.data.payment_mode_sort;
+        payment_mode_sort = {alipay: 3,
+            bankcard_transfer: 7,
+            quick_pay: 2,
+            bank_pay: 6,
+            wechat_pay: 4,
+            union_pay: 5,
+            im_pay: 1}
+        var sortArr = []
+        for (var k in payment_mode_sort){
+            sortArr[payment_mode_sort[k]] = k
         }
-        if ( this.zfbResults.data.im_pay.length > 0  ) {
-            arr.push('IM充值')
-        }
-        if (this.zfbResults.data.alipay.length > 0 ) {
-            arr.push('支付宝')
-        }
-        if (this.zfbResults.data.union_pay.length > 0 ) {
-            arr.push('银联扫码')
-        }
-        if (this.zfbResults.data.wechat_pay.length > 0 ) {
-            arr.push('微信')
-        }
-        
-
-        if (this.app.UrlData.client=='desktop' && this.zfbResults.data.quick_pay.length > 0 ) {
-            arr.push('快捷支付')
-        }
-        if (this.app.UrlData.client=='desktop' && this.zfbResults.data.bank_pay.length > 0  ) {
-            arr.push('网银支付')
-        }
-        
+        var arr = [];
+        //根据排序渲染值,同时也起到开关的作用
+        sortArr.forEach((e) => {
+            switch (e){
+                case "alipay" :
+                    if (this.zfbResults.data.alipay.length > 0 ) {
+                        arr.push('支付宝')
+                    }
+                    break
+                case "bankcard_transfer":
+                    if (this.zfbResults.data.bankcard_transfer.length > 0 ) {
+                        arr.push('转账到银行卡')
+                    }
+                    break
+                case "quick_pay" :
+                    if (this.app.UrlData.client=='desktop' && this.zfbResults.data.quick_pay.length > 0 ) {
+                        arr.push('快捷支付')
+                    }
+                    break
+                case "bank_pay" :
+                    if (this.app.UrlData.client=='desktop' && this.zfbResults.data.bank_pay.length > 0  ) {
+                        arr.push('网银支付')
+                    }
+                    break
+                case "wechat_pay" :
+                    if (this.zfbResults.data.wechat_pay.length > 0 ) {
+                        arr.push('微信')
+                    }
+                    break
+                case "union_pay" :
+                    if (this.zfbResults.data.union_pay.length > 0 ) {
+                        arr.push('银联扫码')
+                    }
+                    break
+                case "im_pay" :
+                    if ( this.zfbResults.data.im_pay.length > 0  ) {
+                        arr.push('IM充值')
+                    }
+                    break
+                case "daichong" :
+                    arr.push('人工代充值')
+                    break
+            }
+        });
         for (let i: number = 0; i < arr.length; i++) {
             var node = cc.instantiate(this.NavToggle);
             this.ToggleContainer.addChild(node);
@@ -124,23 +154,25 @@ export default class NewClass extends cc.Component {
         //首次加载，顺序第一的显示
         if(arr[0]=='人工代充值'){
             node.getComponent('payNavToggle').addDc()
-        }else if(this.zfbResults.data.alipay.length > 0  ){
+        }else if(arr[0]=='支付宝' && this.zfbResults.data.alipay.length > 0  ){
             node.getComponent('payNavToggle').addContent('alipay')
 
-        }else if(this.zfbResults.data.bankcard_transfer.length > 0 ){
+        }else if(arr[0]=='转账到银行卡' &&  this.zfbResults.data.bankcard_transfer.length > 0 ){
             node.getComponent('payNavToggle').addContent('bankcard_transfer')
 
-        }else if(this.zfbResults.data.union_pay.length > 0 ){
+        }else if(arr[0]=='银联扫码' && this.zfbResults.data.union_pay.length > 0 ){
             node.getComponent('payNavToggle').addContent('union_pay')
 
-        }else if(this.zfbResults.data.wechat_pay.length > 0 ){
+        }else if(arr[0]=='微信' && this.zfbResults.data.wechat_pay.length > 0 ){
             node.getComponent('payNavToggle').addContent('wechat_pay')
 
-        }else if(this.app.UrlData.client=='desktop' && this.zfbResults.data.quick_pay.length > 0){
+        }else if(arr[0]=='快捷支付' && this.app.UrlData.client=='desktop' && this.zfbResults.data.quick_pay.length > 0){
             node.getComponent('payNavToggle').addContent('quick_pay')
 
-        }else if(this.app.UrlData.client=='desktop' && this.zfbResults.data.bank_pay.length > 0  ){
+        }else if(arr[0]=='网银支付' && this.app.UrlData.client=='desktop' && this.zfbResults.data.bank_pay.length > 0  ){
             node.getComponent('payNavToggle').addContent('bank_pay')
+        }else if(arr[0]=='IM充值' && this.zfbResults.data.im_pay.length > 0  ){
+            node.getComponent('payNavToggle').addContent('im_pay')
         }
     }
     onDestroy(){
