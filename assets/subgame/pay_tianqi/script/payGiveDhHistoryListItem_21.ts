17629f8002ce2a7bba8601d4e59f84c70694a1ed
tanlangtao <924115911@qq.com>
2022-01-31 04:08:33 +0800

    20220126_万盛娱乐，兑换界面绑定USDT点击确认后无反应

---
+++ assets/subgame/pay_tianqi/script/payGiveDhHistoryListItem_21.ts
@@ -0,0 +1,37 @@
+
+const {ccclass, property} = cc._decorator;
+import { Language_pay } from "./payLanguage_21";
+@ccclass
+export default class NewClass extends cc.Component {
+    @property(cc.Label)
+    created_atLabel: cc.Label = null;
+
+    @property(cc.Label)
+    IdLabel: cc.Label = null;
+
+    @property(cc.Label)
+    amountLabel: cc.Label = null;
+
+    @property(cc.Label)
+    statusLabel: cc.Label = null;
+
+
+    @property
+    public app = null;
+    onLoad () {
+        this.app = cc.find('Canvas/Main').getComponent('payMain_21');
+    }
+
+    public init(data){
+        this.IdLabel.string = data.ReceiveUserId
+        this.amountLabel.string  = this.app.config.toDecimal(data.Amount);
+        if(data.status == 4){
+            this.statusLabel.string  = '已成功'
+        }else if(data.status == 5){
+            this.statusLabel.string  = '已失败'
+        }else{
+            this.statusLabel.string  = '审核中'
+        }
+        this.created_atLabel.string = this.app.config.getTime(data.created_at);
+    }
+}
