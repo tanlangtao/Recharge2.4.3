cf8c6fb5b1f0ec14ecedf9772dbdde11ee82ff22
tanlangtao <924115911@qq.com>
2022-02-05 23:00:05 +0800

    充提系统工程从基础包进行分离

---
+++ assets/subgame/pay_xinsheng/script/payBankCardItem_8.ts
@@ -0,0 +1,22 @@
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property(cc.Label)
+    BankName: cc.Label = null;
+    
+    app = null;
+
+    onLoad () {
+        this.app = cc.find('Canvas/Main').getComponent('payMain_8');
+    }
+    init(info){
+       let Info =  JSON.parse(info)
+       this.BankName.string = Info.bank_name
+       this.label.string = this.app.config.testBankNum(Info.card_num)
+    }
+}
---
+++ assets/subgame/pay_xinsheng/script/payPublicAlert_8.ts
@@ -0,0 +1,43 @@
+
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+    @property()
+    timer = null;
+
+    // LIFE-CYCLE CALLBACKS:
+    public init(data){
+        this.label.string = data.substring(0,80)
+    }
+
+    onLoad () {
+        let app = cc.find('Canvas/Main').getComponent('payMain_8');
+        if(app.UrlData.package_id == 16){
+            this.timer = setTimeout(() => {
+                this.node.destroy()
+            }, 3000)
+        }
+    }
+    // LoadNodes(){
+    //     this.label = this.node.getChildByName('New Layout').children[1].getComponent(cc.Label)
+    // }
+
+    removeSelf(){
+        this.node.destroy()
+    }
+
+    onDestroy(){
+        clearTimeout(this.timer);
+    }
+
+    start () {
+
+    }
+
+    // update (dt) {}
+}
